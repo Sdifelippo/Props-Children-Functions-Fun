d329e7530eae83635bbeba63e55439c653ffe6ef
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-28 21:27:48 -0400

    directory setup and moved components to proper spots

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Header'
+import Footer from './Footer'
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div>This should house Header and Footer components and be able to house any children components.</div>
+    );
+  }
+}
+export default BaseLayout;
